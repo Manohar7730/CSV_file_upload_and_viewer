8e0f4989f46bd6df7f416fb0a5956a996c6505c4
Manohar <manohar20century@gmail.com>
2023-05-30 19:02:09 +0530

    rendered csv file data in file.ejs through csvparser by sending results as data as array

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,69 +1,84 @@
 const CSV = require('../models/csv');
 const fs = require('fs');
 const path = require('path');
+const csv = require('csv-parser');
 
-module.exports.home = async (req,res)=>{
-    try{
+module.exports.home = async (req, res) => {
+    try {
         const csvFiles = await CSV.find({}).sort({ _id: -1 });
-        return res.render('home',{
-            title : 'Home Page',
-            csvFiles : csvFiles
+        return res.render('home', {
+            title: 'Home Page',
+            csvFiles: csvFiles,
+            data: []
         });
-    }catch(err){
+    } catch (err) {
         return res.status(500).send('error in retrieving csv files');
     }
 
 }
 
-module.exports.upload = async (req,res)=>{
-    try{
-        if(req.file){
-            if(req.file.mimetype !== 'text/csv'){
+module.exports.upload = async (req, res) => {
+    try {
+        if (req.file) {
+            if (req.file.mimetype !== 'text/csv') {
                 return res.status(400).send('Only CSV type files are allowed to upload');
             }
             const csv = new CSV({
-                file_name : req.file.filename,
-                original_name : req.file.originalname
+                file_name: req.file.filename,
+                original_name: req.file.originalname
             })
 
             await csv.save();
             return res.redirect('/');
-        }else{
+        } else {
             return res.redirect('/');
         }
-    }catch(err){
+    } catch (err) {
         return res.status(500).send('error in upload the csv files')
     }
 }
 
-module.exports.delete = async (req,res)=>{
-    try{
+module.exports.delete = async (req, res) => {
+    try {
         const fieldId = req.params.id;
-        const deletedCSV = await CSV.findOneAndDelete({_id : fieldId});
-        if(!deletedCSV){
+        const deletedCSV = await CSV.findOneAndDelete({ _id: fieldId });
+        if (!deletedCSV) {
             return res.redirect('/');
         }
-        const filepath = path.join(CSV.filepath,deletedCSV.file_name);
+        const filepath = path.join(CSV.filepath, deletedCSV.file_name);
         await fs.promises.unlink(filepath);
 
         res.redirect('/');
-    }catch(err){
+    } catch (err) {
         return res.status(500).send('Error while deleting the file')
     }
 }
 
-module.exports.render = async(req,res)=>{
+module.exports.render = async (req, res) => {
     const fieldId = req.params.id;
-    try{
+    try {
         const csvFile = await CSV.findById(fieldId);
-        if(!csvFile){
+        if (!csvFile) {
             return res.status(400).send('File not found');
         }
-        res.render('file',{
-            title : 'CSV File',
-            file : csvFile
-        })
-    }catch(err){
+        const filePath = path.join(CSV.filepath, csvFile.file_name);
+
+        const results = [];
+        fs.createReadStream(filePath)
+            .pipe(csv())
+            .on('data', (data) => results.push(data))
+            .on('end', () => {
+                res.render('file', {
+                    title: 'CSV File',
+                    file: csvFile,
+                    data : results
+                });
+            })
+            .on('error', (error) => {
+                console.log(error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            });
+    } catch (err) {
         return res.status(500).send('Cannot view the file')
     }
 }
